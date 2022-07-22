4b3aea0d8ab23a71f06f5726a7861237591087c3
hohoie <105341763+hohoie@users.noreply.github.com>
2022-07-22 11:22:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.set_page_config(
-    page_title="Likelion AI School Dog Team Miniproject",
-    page_icon="🐶",
-    layout="wide",
-)
-st.sidebar.markdown("# Stanford Dogs Data Info🐶")
+st.title('Uber pickups in NYC')
 
-st.markdown('# CV Mini Project')
-st.markdown("## Stanford Dogs Dataset (kaggle) 🐶")
-st.markdown(
-    '#### [Stanford Dogs Dataset link](https://www.kaggle.com/datasets/jessicali9530/stanford-dogs-dataset?datasetId=119698&sortBy=voteCount)')
-st.markdown("강아지 사진을 올리면 무슨 종에 가까운지 알려줍니다!")
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-st.markdown("![Alt Text](https://media.giphy.com/media/kxlDaFDVzTLb2/giphy.gif)")
+@st.cache
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
 
-st.markdown('---')
+data_load_state = st.text('Loading data...')
+data = load_data(10000)
+data_load_state.text("Done! (using st.cache)")
 
-st.markdown('팀장: 주소리')
-st.markdown('팀원: 이주승, 김재석')
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+
+# Some number in the range 0-23
+hour_to_filter = st.slider('hour', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.map(filtered_data)
