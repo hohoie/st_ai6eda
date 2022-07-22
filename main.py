79099792464ae0199f3d9258b1a2b111b51982e9
hohoie <105341763+hohoie@users.noreply.github.com>
2022-07-22 11:20:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 
-st.title('Uber pickups in NYC')
+st.set_page_config(
+    page_title="Likelion AI School Dog Team Miniproject",
+    page_icon="🐶",
+    layout="wide",
+)
+st.sidebar.markdown("# Stanford Dogs Data Info🐶")
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+st.markdown('# CV Mini Project')
+st.markdown("## Stanford Dogs Dataset (kaggle) 🐶")
+st.markdown(
+    '#### [Stanford Dogs Dataset link](https://www.kaggle.com/datasets/jessicali9530/stanford-dogs-dataset?datasetId=119698&sortBy=voteCount)')
+st.markdown("강아지 사진을 올리면 무슨 종에 가까운지 알려줍니다!")
 
+st.markdown("![Alt Text](https://media.giphy.com/media/kxlDaFDVzTLb2/giphy.gif)")
+
+st.markdown('---')
+
+st.markdown('팀장: 주소리')
+st.markdown('팀원: 이주승, 김재석')
